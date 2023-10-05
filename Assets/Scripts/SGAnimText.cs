499be4b59a72a350016424acc2af787acf0c953e
YasuoSkywalker <91738819+YasuoSkywalker@users.noreply.github.com>
2023-10-05 22:38:28 +0800

    添加文本动画SGAnimText

---
+++ Assets/Scripts/SGAnimText.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+//TODO:字体放大效果改为使用textInfo.characterInfo[i].pointSize修改
+//TODO:添加更多的效果
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class SGAnimText : MonoBehaviour
+{
+    //TextMeshProUGUI组件
+    private TextMeshProUGUI tmpUGUI;
+    //TextMeshProUGUI的文本
+    private string tmpUGUIText;
+
+    #region 打字效果
+    [SerializeField]
+    [Header("使用打字效果")]
+    [Tooltip("勾选后，将会以打字的效果逐个展示字符")]
+    private bool useTypeEffect = false;
+    [SerializeField]
+    [Header("是否对全部文本生效")]
+    private bool applyTypeToAllText = false;
+    [SerializeField]
+    [Header("打字效果生效的起始位置")]
+    private int typeStartIndex = 0;
+    [SerializeField]
+    [Header("打字效果生效的结束位置")]
+    private int typeEndIndex = 0;
+    [SerializeField]
+    [Header("打字间隔")]
+    private float typeSpace = 0.2f;
+    #endregion
+
+    #region 放大效果
+    [SerializeField]
+    [Header("使用放大效果")]
+    [Tooltip("勾选后，会将设定范围内的字体放大")]
+    private bool useBiggerEffect = false;
+    [SerializeField]
+    [Header("是否对全部文本生效")]
+    private bool applyBiggerToAllText = false;
+    [SerializeField]
+    [Header("打字效果生效的起始位置")]
+    private int biggerStartIndex = 0;
+    [SerializeField]
+    [Header("打字效果生效的结束位置")]
+    private int biggerEndIndex = 0;
+    [SerializeField]
+    [Header("放大倍率")]
+    [Min(0f)]
+    private float biggerRate = 1.2f;
+    #endregion
+
+    private void Awake()
+    {
+        tmpUGUI = GetComponent<TextMeshProUGUI>();
+        tmpUGUIText = tmpUGUI.text;
+        tmpUGUI.ForceMeshUpdate();
+        HandleTypeEffect();
+        HandleBiggerEffect();
+    }
+
+    /// <summary>
+    /// 处理打字效果
+    /// </summary>
+    private void HandleTypeEffect()
+    {
+        if (!useTypeEffect) return;
+        //如果勾选了对所有文字生效
+        if(applyTypeToAllText)
+        {
+            //先清空TMP的text
+            tmpUGUI.text = "";
+            //将整段文字传入处理打字效果的协程函数中
+            StartCoroutine(TypeEffectCoroutine(tmpUGUIText));
+        }
+        else
+        {
+            tmpUGUI.text = "";
+            //数据正确性检查
+            if(typeStartIndex >= tmpUGUIText.Length || typeStartIndex < 0)
+            {
+                Debug.LogError("typeStartIndex Out Of Range!");
+                return;
+            }
+            if(typeEndIndex >= tmpUGUIText.Length || typeEndIndex < 0)
+            {
+                Debug.LogWarning("typeEndIndex Out Of Range!");
+                typeEndIndex = tmpUGUIText.Length - 1;
+            }
+            if (typeEndIndex < typeStartIndex)
+            {
+                Debug.LogError("typeEndIndex must larger than typeStartIndex");
+                return;
+            }
+            //打字效果之前部分
+            tmpUGUI.text += tmpUGUIText.Substring(0, typeStartIndex);
+            //打字效果，onFinish传入打字效果之后部分
+            StartCoroutine(TypeEffectCoroutine(tmpUGUIText.Substring(typeStartIndex, typeEndIndex - typeStartIndex + 1),
+                ()=> {
+                    if(typeEndIndex+1 < tmpUGUIText.Length)
+                        tmpUGUI.text += tmpUGUIText.Substring(typeEndIndex + 1, tmpUGUIText.Length - typeEndIndex-1);
+                }));
+        }
+    }
+
+    /// <summary>
+    /// 处理打字效果协程
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="onFinish"></param>
+    /// <returns></returns>
+    IEnumerator TypeEffectCoroutine(string text,Action onFinish = null)
+    {
+        int index = 0;
+        //逐个输出文字
+        while(index < text.Length)
+        {
+            tmpUGUI.text += text[index++];
+            yield return new WaitForSeconds(typeSpace);
+        }
+        //打字效果结束后，调用onFinish Action，记得判空
+        onFinish?.Invoke();
+    }
+
+    /// <summary>
+    /// 处理放大效果
+    /// </summary>
+    private void HandleBiggerEffect()
+    {
+        if (!useBiggerEffect) return;
+        if(applyBiggerToAllText)
+        {
+            tmpUGUI.fontSize = tmpUGUI.fontSize * biggerRate;
+        }
+        else
+        {
+            //数据正确性检查
+            if (biggerStartIndex >= tmpUGUIText.Length || biggerStartIndex < 0)
+            {
+                Debug.LogError("biggerStartIndex Out Of Range!");
+                return;
+            }
+            if (biggerEndIndex >= tmpUGUIText.Length || biggerEndIndex < 0)
+            {
+                Debug.LogWarning("biggerEndIndex Out Of Range!");
+                typeEndIndex = tmpUGUIText.Length - 1;
+            }
+            if (biggerEndIndex < biggerStartIndex)
+            {
+                Debug.LogError("biggerEndIndex must larger than biggerStartIndex");
+                return;
+            }
+            /*for(int i=biggerStartIndex;i<=biggerEndIndex;i++)
+            {
+                tmpUGUI.textInfo.characterInfo[i].pointSize = tmpUGUI.textInfo.characterInfo[i].pointSize*biggerRate;
+                tmpUGUI.ForceMeshUpdate();
+            }*/
+            //根据规则生成带有富文本的字符串
+            float size = tmpUGUI.fontSize * biggerRate;
+            string effectText = "";
+            for(int i=0;i<tmpUGUIText.Length;i++)
+            {
+                if(i == biggerStartIndex)
+                {
+                    effectText += ("<size=" + size + ">");
+                }
+                effectText += tmpUGUIText[i];
+                if (i == biggerEndIndex)
+                {
+                    effectText += "</size>";
+                }
+            }
+            //应用字符串
+            tmpUGUIText = effectText;
+            tmpUGUI.text = effectText;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # SGUIToolBox
-StarLightGrimmer‘s Unity UI ToolBox
+**S**tarLight**G**rimmer‘s Unity UI ToolBox
+
+星辰微光的UI工具箱
+
+程序化UI练习项目
+
+各脚本位于`Asset/Scripts`文件夹中，该文件夹下的test文件夹为测试代码
+
+## SGButton
+
+自定义的Button组件，在原有Button的基础上新增了一些内容：
+
+- 视觉上跳过选中状态
+- 鼠标点击、进入、长按时应用Scale变化（可选）
+- 鼠标悬停
+- 鼠标长按
+- 鼠标进入/离开
+
+## SGProgressBar
+
+自定义的进度条组件
+
+- 显示形式（整数，小数，百分率）
+- 可设置进度条变化速率
+- 可设置不同进度的颜色变化
+
+## SGAnimText
+
+自定义的文本动画组件，需要附加在带有`TextMeshProUGUI`的物体上
+
+- 打字机效果
+- 部分字体放大
+- 更多效果待开发
+
+## TODO
+
+- [ ] 为每个Event添加AddListener方法和RemoveListener方法
+- [ ] 按钮分开长按和点击
+- [ ] SGAnimText部分字体放大部分改用`textInfo.characterInfo[i].pointSize`
+- [ ] 为SGAnimText添加部分字体颜色变化、部分字体动画等效果
